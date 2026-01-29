d1c052dd37ae534d2dd139d19c307253c4ad104c
Jonas Ransjö <jonas.ransjo@omegapoint.se>
2026-01-29 22:59:12 +0100

    Add .dir-locals.el to make rg work

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((rg-command-line-flags . ("--null --color=never --max-columns=200 --smart-case --no-heading --with-filename --line-number --hidden --glob !.git"))
+         (consult-ripgrep-args . "rg --null --color=never --max-columns=200 --smart-case --no-heading --with-filename --line-number --hidden --glob !.git")
+         (magit-todos-rg-extra-args . ("--hidden" "--glob" "!.git")))))
